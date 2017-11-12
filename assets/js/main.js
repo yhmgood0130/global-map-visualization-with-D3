a6ab9625f55d3781a820bc822ff2961a7418a2ea
Hyunmo <yhmgood0130@gmail.com>
2017-11-12 02:39:01 -0700

    Changed src

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,10 @@ var lengthOfCountry = 0;
 var isSameCountry = true;
 var nameOfCountry = "";
 var colors = [];
-const IHME_CSV = "assets/data/IHME_GBD_2013_OBESITY_PREVALENCE_1990_2013_Y2014M10D08.csv"
+// const IHME_CSV = "assets/data/IHME_GBD_2013_OBESITY_PREVALENCE_1990_2013_Y2014M10D08.csv"
 
 $(document).ready(function(){
-  d3.csv(IHME_CSV, function(err, data) {
+  d3.csv("/static/IHME_GBD_2013_OBESITY_PREVALENCE_1990_2013_Y2014M10D08.csv", function(err, data) {
     var config = {"data0":"location_name","data1":"mean",
                 "label0":"label 0","label1":"label 1","color0":"#a7faad","color1":"#0d6613",
                 "width":1024,"height":1024}
